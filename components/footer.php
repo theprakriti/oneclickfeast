0717eb49480d062804f4a5a494aa458709ca16a2
theprakriti <prakritisharma057@gmail.com>
2024-03-04 23:25:24 +0545

    msg

---
+++ components/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+   <hr>
+   <div class="contact-info">
+      <div class="contact-item">
+         <img src="images/emailicon.png" alt="Email Icon">
+         <h3>Our Email</h3>
+         <a href="mailto:oneclickfeast@gmail.com">oneclickfeast@gmail.com</a>
+      </div>
+
+      <div class="contact-item">
+         <img src="images/clockicon.png" alt="Clock Icon">
+         <h3>Opening Hours</h3>
+         <p>07:00 AM to 08:00 PM</p>
+      </div>
+
+      <div class="contact-item">
+         <img src="images/mapicon.png" alt="Map Icon">
+         <h3>Our Address</h3>
+         <a href="#">Narayanghat, Chitwan</a>
+      </div>
+
+      <div class="contact-item">
+         <img src="images/phoneicon.png" alt="Phone Icon">
+         <h3>Our Number</h3>
+         <a href="tel:1234567890">+9779812345678</a>
+      </div>
+   </div>
+
+   <div class="credit">
+      <p>&copy; copyright @ <?= date('Y'); ?> by <span>OneClick Feast</span> | all rights reserved!</p>
+   </div>
+</footer>
