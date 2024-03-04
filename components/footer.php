32f6bb8d9008718cdab048cb342098cfcc68a9f2
Bipin Paudel <bipo_bca2076@lict.edu.np>
2024-03-04 22:14:26 +0545

    code on footer

--- components/footer.php
+++ components/footer.php
@@ -1,28 +1,30 @@
 <footer class="footer">
 
-   <section class="grid">
+<hr>
 
-      <div class="box">
-         <img src="images/emailicon.png" alt="">
+   <section style="display: flex;justify-content: space-around;background-color: whitesmoke;width: 100vw!important;">
+
+      <div style="display: flex;flex-direction: column;align-items: center;">
+         <img src="images/emailicon.png" style="height: 30px;width: 20px;object-fit: contain;" alt="">
          <h3>Our Email</h3>
          <a href="mailto:abc@gmail.com">abc@gmail.com</a>
          
       </div>
 
-      <div class="box">
-         <img src="images/clockicon.png" alt="">
+      <div style="display: flex;flex-direction: column;align-items: center;">
+         <img src="images/clockicon.png" style="height: 30px;width: 20px;object-fit: contain;" alt="">
          <h3>Opening Hours</h3>
          <p>07:00AMto 08:00PM</p>
       </div>
 
-      <div class="box">
-         <img src="images/mapicon.png" alt="">
+      <div style="display: flex;flex-direction: column;align-items: center;">
+         <img src="images/mapicon.png" style="height: 30px;width: 20px;object-fit: contain;" alt="">
          <h3>Our Address</h3>
          <a href="#">Narayanghat, Chitwan</a>
       </div>
 
-      <div class="box">
-         <img src="images/phoneicon.png" alt="">
+      <div style="display: flex;flex-direction: column;align-items: center;">
+         <img src="images/phoneicon.png" style="height: 30px;width: 20px;object-fit: contain;" alt="">
          <h3>Our Number</h3>
          <a href="tel:1234567890">123-456-7890</a>
          
@@ -30,7 +32,9 @@
 
    </section>
 
-   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>PSS</span> | all rights reserved!</div>
+   <div class="credit">
+      <p style="font-size: medium;">&copy; copyright @ <?= date('Y'); ?> by <span>OneClick Feast</span> | all rights reserved!</p>
+   </div>
 
 </footer>
 
